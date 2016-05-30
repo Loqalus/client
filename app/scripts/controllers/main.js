fd1a2b25cd845d7803ba2179d32a1cbcbb489da5
Jmbrace <jmbrace@ilstu.edu>
2016-05-30 15:18:38 -0500

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loqalusClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the loqalusClientApp
+ */
+angular.module('loqalusClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
